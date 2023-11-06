b5eef13e8cf7acb4031d617259c848dc88d2f9bd
MichaelElmore1 <135364586+MichaelElmore1@users.noreply.github.com>
2023-11-06 16:13:42 -0600

    Add Bluetooth connectivity

--- sensorhub-android-kromek-d5/src/main/java/org/sensorhub/impl/sensor/kromek/d5/D5Sensor.java
+++ sensorhub-android-kromek-d5/src/main/java/org/sensorhub/impl/sensor/kromek/d5/D5Sensor.java
@@ -12,9 +12,18 @@
 
 package org.sensorhub.impl.sensor.kromek.d5;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Context;
+
 import org.sensorhub.impl.sensor.AbstractSensorModule;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.Set;
+import java.util.UUID;
 
 /**
  * Main driver class for the Kromek D5 sensor. This class is responsible for initializing the
@@ -26,6 +35,7 @@ import org.slf4j.LoggerFactory;
 public class D5Sensor extends AbstractSensorModule<D5Config> {
     private static final Logger logger = LoggerFactory.getLogger(D5Sensor.class);
     D5Output output;
+    Context context;
 
     public D5Sensor() {
         logger.info("Creating D5 Sensor");
@@ -54,11 +64,59 @@ public class D5Sensor extends AbstractSensorModule<D5Config> {
     public void doStart() {
         logger.info("Starting D5 Sensor");
 
-        output.setData();
+        context = config.getContext();
+
+        BluetoothDevice device = getBluetoothDevice("D5M");
+        if (device == null) {
+            logger.error("D5 Device not found");
+            return;
+        }
+        UUID uuid = device.getUuids()[0].getUuid();
+
+        logger.info("D5 Device found: " + device.getName() + " " + device.getAddress() + " " + uuid);
+
+        try (BluetoothSocket socket = device.createRfcommSocketToServiceRecord(uuid)) {
+            logger.info("Socket created");
+            socket.connect();
+            logger.info("Socket connected");
+
+        } catch (IOException e) {
+            logger.error("Socket connection failed", e);
+        }
     }
 
     @Override
     public void doStop() {
         logger.info("Stopping D5 Sensor");
+    }
+
+    public BluetoothDevice getBluetoothDevice(String deviceNamePrefix) {
+        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (bluetoothAdapter == null) {
+            logger.error("Device does not support Bluetooth");
+        } else {
+            if (bluetoothAdapter.isEnabled()) {
+                // Get a set of currently paired devices
+                Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
+                if (pairedDevices.size() > 0) {
+                    // There are paired devices. Get the name and address of each paired device.
+                    for (BluetoothDevice device : pairedDevices) {
+                        String deviceName = device.getName();
+                        String deviceHardwareAddress = device.getAddress(); // MAC address
+                        logger.info("Paired device: " + deviceName + " " + deviceHardwareAddress);
+
+                        if (deviceName.startsWith(deviceNamePrefix)) {
+                            logger.info("Found device: " + deviceName);
+                            bluetoothAdapter.cancelDiscovery();
+                            return device;
+                        }
+                    }
+                }
+            } else {
+                logger.error("Bluetooth is not enabled");
+            }
+        }
+
+        return null;
     }
 }
