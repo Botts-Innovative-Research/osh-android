58e93c2f44a554038a0579d0f462eb9f3cc78b9a
Michael Elmore <135364586+MichaelElmore1@users.noreply.github.com>
2023-12-12 11:55:09 -0600

    Remove unnecessary logging

--- sensorhub-android-kromek-d5/src/main/java/org/sensorhub/impl/sensor/kromek/d5/reports/KromekDetectorRadiometricsV1Report.java
+++ sensorhub-android-kromek-d5/src/main/java/org/sensorhub/impl/sensor/kromek/d5/reports/KromekDetectorRadiometricsV1Report.java
@@ -22,7 +22,6 @@ import net.opengis.swe.v20.DataBlock;
 import net.opengis.swe.v20.DataRecord;
 import net.opengis.swe.v20.DataType;
 
-import org.sensorhub.impl.sensor.kromek.d5.D5Sensor;
 import org.vast.swe.SWEHelper;
 
 import java.nio.ByteBuffer;
@@ -80,7 +79,6 @@ public class KromekDetectorRadiometricsV1Report extends SerialReport {
         doseRate = doseRate * 1000000;
         // Sometimes the dose rate is super high for some reason. If it is, set it to 0 so it doesn't mess up the graph.
         if (doseRate > 100) {
-            D5Sensor.logger.info("Dose rate is too high: " + doseRate + " uSv/h. Setting to 0.");
             doseRate = 0;
         }
         doseUserAccumulated = bytesToFloat(payload[20], payload[21], payload[22], payload[23]);
