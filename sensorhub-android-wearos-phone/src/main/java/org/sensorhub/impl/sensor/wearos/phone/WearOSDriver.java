b92d266c23ac89e7bcf05c7df10727387df2b123
Michael Elmore <135364586+MichaelElmore1@users.noreply.github.com>
2024-04-15 14:17:02 -0500

    Change Android location point color to green

--- sensorhub-android-wearos-phone/src/main/java/org/sensorhub/impl/sensor/wearos/phone/WearOSDriver.java
+++ sensorhub-android-wearos-phone/src/main/java/org/sensorhub/impl/sensor/wearos/phone/WearOSDriver.java
@@ -397,7 +397,7 @@ public class WearOSDriver extends AbstractSensorModule<WearOSConfig> implements 
                             List<String> observations = ObservationsItems.getObservationsJSon(auth, config.gpsDataLocation.gpsHost, dataStream.getId());
                             for (String observation : observations) {
                                 GPSAndroidLocationResult data = GPSAndroidLocationResult.fromJson(observation);
-                                locationData.add(new GPSDataPoint(data.getLat(), data.getLon(), "blue"));
+                                locationData.add(new GPSDataPoint(data.getLat(), data.getLon(), "green"));
                             }
                         }
                     }
