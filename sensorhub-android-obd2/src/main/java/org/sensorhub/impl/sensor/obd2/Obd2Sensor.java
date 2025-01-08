2cf44f35b7568013b88a44be1c02f65f78df1bda
Philip Khaisman <philip.khaisman@botts-inc.com>
2025-01-08 14:21:27 -0500

    Implement connection for bluetooth classic instead of ble

--- sensorhub-android-obd2/src/main/java/org/sensorhub/impl/sensor/obd2/Obd2Sensor.java
+++ sensorhub-android-obd2/src/main/java/org/sensorhub/impl/sensor/obd2/Obd2Sensor.java
@@ -7,6 +7,8 @@ import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothGatt;
 import android.bluetooth.BluetoothGattCallback;
 import android.bluetooth.BluetoothManager;
+import android.bluetooth.BluetoothProfile;
+import android.bluetooth.BluetoothSocket;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.os.Build;
@@ -18,12 +20,19 @@ import org.sensorhub.impl.sensor.AbstractSensorModule;
 import org.sensorhub.api.common.SensorHubException;
 
 import java.util.Set;
+import java.util.UUID;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class Obd2Sensor extends AbstractSensorModule<Obd2Config> {
+    private static final UUID SPP_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+
     private String deviceName;
     private Context context;
     private BluetoothAdapter btAdapter;
-    private BluetoothGatt btGatt;
+    private BluetoothSocket btSocket; // TODO Should this be an attribute?
+    // private BluetoothGatt btGatt;
     Obd2Output output;
     private boolean btConnected = false;
 
@@ -33,14 +42,13 @@ public class Obd2Sensor extends AbstractSensorModule<Obd2Config> {
 
     @Override
     protected void doInit() throws SensorHubException {
-        // TODO Do I need this
-        // super.doInit();
+        // TODO Do I to call super.doInit()?
 
         // set IDs
         this.xmlID = "OBD2_" + Build.SERIAL;
         this.uniqueID = Obd2Config.getUid();
 
-        // set up bluetooth adapter
+        // get the android's bluetooth adapter
         context = SensorHubService.getContext();
         final BluetoothManager bluetoothManager = (BluetoothManager) context.getSystemService(Activity.BLUETOOTH_SERVICE);
         btAdapter = bluetoothManager.getAdapter();
@@ -49,22 +57,10 @@ public class Obd2Sensor extends AbstractSensorModule<Obd2Config> {
             throw new SensorException("Could not get bluetooth adapter, unable to initiate.");
         }
 
-        // TODO The STE sensor uses location data. I need to determine if I need it
-
-        // init output(s)
-        output = new Obd2Output(this);
-        output.doInit();
-        addOutput(output, false);
-    }
-
-    @Override
-    protected void doStart() throws SensorHubException {
-        // TODO Do I need to call super.doStart()?
-
-        Set<BluetoothDevice> devices = btAdapter.getBondedDevices();
         BluetoothDevice device = null;
+        Set<BluetoothDevice> devices = btAdapter.getBondedDevices();
 
-        // find the device
+        // find the bluetooth device
         for (BluetoothDevice d : devices) {
             if (d.getName().equals(deviceName)) {
                 device = d;
@@ -75,19 +71,44 @@ public class Obd2Sensor extends AbstractSensorModule<Obd2Config> {
             throw new SensorException("Could not find bluetooth device, unable to start.");
         }
 
-        // request bluetooth permissions
-        if (context.checkSelfPermission(Manifest.permission.BLUETOOTH) == PackageManager.PERMISSION_DENIED) {
-            ActivityCompat.requestPermissions((Activity) context, new String[]{Manifest.permission.BLUETOOTH}, 1);
+        // create a bluetooth socket
+        try {
+            btSocket = device.createRfcommSocketToServiceRecord(SPP_UUID);
+        } catch (IOException e) {
+            // TODO Is this what I want to happen if creating the socket fails?
+            throw new SensorException("Could not create client socket", e);
         }
 
-        // connect to the device
-        btGatt = device.connectGatt(context, true, gattCallback);
+        // TODO Do I need to use location data?
+
+        // init osh output(s)
+        output = new Obd2Output(this);
+        output.doInit();
+        addOutput(output, false);
+    }
+
+    @Override
+    protected void doStart() throws SensorHubException {
+        // TODO Do I need to call super.doStart()?
+
+        btAdapter.cancelDiscovery();
+
+        // connect to the bluetooth device
+        try {
+            btSocket.connect();
+        } catch (IOException connectException) {
+            try {
+                btSocket.close();
+            } catch (IOException e) {
+                // TODO Is this what I want to happen if connecting to the socket fails?
+                throw new SensorException("Could not close the client socket", e);
+            }
+        }
     }
 
     @Override
     protected void doStop() throws SensorHubException {
-        btGatt.disconnect();
-        btGatt.close();
+        // TODO Close the socket. Here and/or elsewhere too?
     }
 
     @Override
@@ -110,5 +131,21 @@ public class Obd2Sensor extends AbstractSensorModule<Obd2Config> {
         return btConnected;
     }
 
-    private BluetoothGattCallback gattCallback = new BluetoothGattCallback() {};
+    private BluetoothGattCallback gattCallback = new BluetoothGattCallback() {
+        @Override
+        public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
+            if (newState == BluetoothProfile.STATE_CONNECTED) {
+                btConnected = true;
+                gatt.discoverServices();
+            } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
+                btConnected = false;
+                // TODO What should I do if connection is lost?
+            }
+        }
+
+        @Override
+        public void onServicesDiscovered(BluetoothGatt gatt, int status) {
+
+        }
+    };
 }
