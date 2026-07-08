2a4a81daf70b1781cb82e35750f45b56f0123f16
kalynstricklin <kalynstricklin@botts-inc.com>
2026-07-08 17:03:14 -0500

    Add 3 main screens

---
+++ sensorhub-android-app/src/org/sensorhub/android/ui/screens/SensorsScreen.kt
@@ -0,0 +1,23 @@
+package org.sensorhub.android.ui.screens
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+
+@Composable
+fun SensorsScreen() {
+    Box (modifier = Modifier
+        .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ){
+        Text(
+            text = "Sensors Screen",
+            style = MaterialTheme.typography.headlineLarge
+        )
+    }
+}
