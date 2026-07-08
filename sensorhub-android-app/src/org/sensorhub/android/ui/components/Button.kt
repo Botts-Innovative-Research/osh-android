f13ff1415fa755054e23129a9a66246fc2236436
kalynstricklin <kalynstricklin@botts-inc.com>
2026-07-08 16:56:52 -0500

    Add single select segmented buttons

--- sensorhub-android-app/src/org/sensorhub/android/ui/components/Button.kt
+++ sensorhub-android-app/src/org/sensorhub/android/ui/components/Button.kt
@@ -9,8 +9,16 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import org.sensorhub.android.ui.theme.*
+import androidx.compose.material3.SingleChoiceSegmentedButtonRow
+import androidx.compose.material3.SegmentedButton
+
 
 private val PillShape = RoundedCornerShape(25)
 
@@ -112,6 +120,37 @@ fun OSHTextButton(
     }
 }
 
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun OSHSegmentedButton(
+    modifier: Modifier = Modifier,
+    options: List<String>
+) {
+    var selectedIndex by remember { mutableIntStateOf(0) }
+
+    SingleChoiceSegmentedButtonRow(modifier = modifier.fillMaxWidth()) {
+        options.forEachIndexed { index, label ->
+            SegmentedButton(
+                modifier = Modifier.weight(1f),
+                shape = SegmentedButtonDefaults.itemShape(
+                    index = index,
+                    count = options.size
+                ),
+                onClick = { selectedIndex = index },
+                selected = index == selectedIndex,
+                label = { Text(label, maxLines = 1) },
+                colors = SegmentedButtonDefaults.colors(
+                    activeContainerColor = Primary,
+                    activeContentColor = OnPrimary,
+                    activeBorderColor = Primary
+                ),
+                icon = {},
+            )
+        }
+    }
+}
+
 @Preview(showBackground = true, backgroundColor = 0xFF121212)
 @Composable
 private fun ButtonPreview() {
@@ -124,5 +163,15 @@ private fun ButtonPreview() {
             Spacer(modifier = Modifier.height(8.dp))
             OSHTextButton(onClick = {}, text = "Text Button")
         }
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFF121212)
+@Composable
+private fun SegmentedButtonPreview() {
+    OSHTheme {
+        OSHSegmentedButton(
+            options = listOf("CS API Client", "SOS-T Client")
+        )
     }
 }
