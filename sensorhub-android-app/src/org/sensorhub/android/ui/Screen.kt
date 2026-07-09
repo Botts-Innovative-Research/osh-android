369fcadcefdd16c3efca87dd63b87f54c1c1e94a
kalynstricklin <kalynstricklin@botts-inc.com>
2026-07-08 21:54:40 -0500

    init for app status

--- sensorhub-android-app/src/org/sensorhub/android/ui/Screen.kt
+++ sensorhub-android-app/src/org/sensorhub/android/ui/Screen.kt
@@ -4,4 +4,5 @@ sealed class Screen(val route: String) {
     object Dashboard : Screen("dashboard_screen")
     object Servers : Screen("servers_screen")
     object Sensors : Screen("sensors_screen")
+    object AppStatus : Screen("app_status_screen")
 }
