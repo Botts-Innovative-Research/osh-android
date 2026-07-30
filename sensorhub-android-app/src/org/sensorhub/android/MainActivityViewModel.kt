c0f46eef0eaae17984baaccb373bdcf5b224ee40
kalynstricklin <kalynstricklin@botts-inc.com>
2026-07-30 14:26:24 -0500

    remove unnecessary activities for help, status and prefs

--- sensorhub-android-app/src/org/sensorhub/android/MainActivityViewModel.kt
+++
@@ -1,9 +0,0 @@
-package org.sensorhub.android
-
-import android.app.Application
-import androidx.lifecycle.AndroidViewModel
-
-class MainActivityViewModel(application: Application) : AndroidViewModel(application) {
-
-
-}
