92b66a5ef573c5f572a9d91d3e977393df68ba80
kalynstricklin <kalynstricklin@botts-inc.com>
2026-08-21 11:11:00 -0500

    updated AGP to 8.2.2

--- sensorhub-android-app/src/org/sensorhub/android/DashboardFragment.java
+++ sensorhub-android-app/src/org/sensorhub/android/DashboardFragment.java
@@ -386,7 +386,7 @@ public class DashboardFragment extends Fragment implements TextureView.SurfaceTe
         displayCallback = new Runnable() {
             public void run() {
                 displayStatus();
-                videoInfoArea.setText(Html.fromHtml(videoInfoText.toString()));
+                videoInfoArea.setText(Html.fromHtml(videoInfoText.toString(), Html.FROM_HTML_MODE_LEGACY));
                 displayHandler.postDelayed(this, 1000);
             }
         };
